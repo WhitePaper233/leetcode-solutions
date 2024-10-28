341e26830d9c50ebc0b71c940a8ab64801403965
WhitePaper233 <1423634265@qq.com>
2024-10-28 21:12:16 +0800

    https://leetcode.cn/problems/combination-sum/description/

--- 39.组合总和.cpp
+++ 39.组合总和.cpp
@@ -14,52 +14,32 @@ class Solution {
 public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<vector<int>> ans {};
-        solve(candidates, ans, {}, target);
+        solve(candidates, ans, {}, target, 0, 0);
         return ans;
     }
 
-    void solve(const vector<int>& candidates, vector<vector<int>>& ans, vector<int> curr, int target) {
-        int currSum = sum(curr);
-        for (int num : candidates) {
+    void solve(
+        const vector<int>& candidates, 
+        vector<vector<int>>& ans, 
+        vector<int> curr,
+        int target,
+        int currIndex,
+        int currSum
+    ) {
+        for (int i = currIndex; i < candidates.size(); ++i) {
+            int num = candidates[i];
             if (currSum + num == target) {
                 vector<int> next = curr;
                 next.push_back(num);
-                if (!checkDuplicated(ans, next)) ans.push_back(next);
+                ans.push_back(next);
                 continue;
             }
             if (currSum + num < target) {
                 vector<int> next = curr;
                 next.push_back(num);
-                solve(candidates, ans, next, target);
+                solve(candidates, ans, next, target, i, currSum + num);
             }
         }
-    }
-
-    int sum(vector<int>& vec) {
-        int sum = 0;
-        for (auto num : vec) {
-            sum += num;
-        }
-        return sum;
-    }
-
-    bool checkDuplicated(vector<vector<int>>& ans, vector<int>& vec) {
-        sort(vec.begin(), vec.end());
-
-        for (auto arr : ans) {
-            if (arr.size() != vec.size()) continue;
-
-            bool flag = true;
-            for (int i = 0; i < vec.size(); ++i) {
-                if (arr[i] != vec[i]) {
-                    flag = false;
-                    break;
-                };
-            }
-
-            if (flag) return true;
-        }
-        return false;
     }
 };
 // @lc code=end
